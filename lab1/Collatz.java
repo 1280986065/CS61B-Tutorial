61afd3dd30c4e0e9ab218d55b6302289dfb0e655
1280986065 <1280986065@qq.com>
2022-05-31 19:18:01 +0800

    第一次提交

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -4,7 +4,7 @@
 public class Collatz {
 
     /** Buggy implementation of nextNumber! */
-    public static int nextNumber(int n) {
+/**public static int nextNumber(int n) {
         if (n  == 128) {
             return 1;
         } else if (n == 5) {
@@ -12,8 +12,16 @@ public class Collatz {
         } else {
             return n * 2;
         }
+    }*/
+    public static int nextNumber(int n) {
+        if (n  == 1) {
+            return n;
+        } else if (n % 2 == 0) {
+            return n / 2;
+        } else {
+            return 3 * n +1;
+        }
     }
-
     public static void main(String[] args) {
         int n = 5;
         System.out.print(n + " ");
