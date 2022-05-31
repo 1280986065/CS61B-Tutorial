bfdbcff4ffbadfdbc043dd7b89b8c3b45923a4d9
1280986065 <106468084+1280986065@users.noreply.github.com>
2022-05-31 22:43:30 +0800

    Update Collatz.java

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -7,11 +7,7 @@ public class Collatz {
 /**public static int nextNumber(int n) {
         if (n  == 128) {
             return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
-        } else {
-            return n * 2;
-        }
+        } else return n % 2 == 1 ? 3 * n + 1 : n / 2;
     }*/
     public static int nextNumber(int n) {
         if (n  == 1) {
@@ -19,7 +15,7 @@ public class Collatz {
         } else if (n % 2 == 0) {
             return n / 2;
         } else {
-            return 3 * n +1;
+            return 3 * n + 1;
         }
     }
     public static void main(String[] args) {
